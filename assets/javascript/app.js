28c59374cb1e944c0c8eeb16705eae3ac41b730a
Benjamin Brier <ben.brier14@gmail.com>
2019-09-10 12:34:05 -0400

    created a function to display question and choices

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,29 @@
 //Create variables that will be used for game
 const triviaQuestions = [
 {question: "Which character is the strongest?",
-answers: {a: "Goku", b: "Beerus", c: "Frieza"},
-correctAnswer: "b"
+options: ["Goku", "Beerus", "Frieza"],
+correctAnswer: "Beerus"
 },
 {question: "What wearable item is used to detect power levels?",
-answers: {a: "Scouter", b: "Radar", c: "Gauger"},
-correctAnswer: "a"
+options: ["Scouter", "Radar", "Gauger"],
+correctAnswer: "Scouter"
 },
 {question: "Who is Buu's best friend?",
-answers: {a: "Bulma", b: "Raditz", c: "Hercule"},
-correctAnswer: "b"
+options: ["Bulma", "Raditz", "Hercule"],
+correctAnswer: "Hercule"
 }]
 
-console.log( $ );
+var counter = 30;
+var score = 0;
+var missed = 0;
+var currentQuestion = 0;
+var timer; 
+
+//function needed to put the questions and options list on the screen
+function showQuestion() {
+    var question = triviaQuestions[currentQuestion].question;
+    var choices = triviaQuestions[currentQuestion].options;
+    $('#time').html(counter);
+    $('#questionSection').html(question);
+
+}
