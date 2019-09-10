a06ad8a23c0830041ec751a8fb0a0ff5ac69d741
Benjamin Brier <ben.brier14@gmail.com>
2019-09-10 11:55:01 -0400

    created jumbotron with start button

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+//Create variables that will be used for game
+const triviaQuestions = [
+{question: "Which character is the strongest?",
+answers: {a: "Goku", b: "Beerus", c: "Frieza"},
+correctAnswer: "b"
+},
+{question: "What wearable item is used to detect power levels?",
+answers: {a: "Scouter", b: "Radar", c: "Gauger"},
+correctAnswer: "a"
+},
+{question: "Who is Buu's best friend?",
+answers: {a: "Bulma", b: "Raditz", c: "Hercule"},
+correctAnswer: "b"
+}]
+
+console.log( $ );
